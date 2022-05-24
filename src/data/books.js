ae4063d099e0b0b443702e38bd026e81d18dfc52
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-05-23 23:31:47 -0500

    Create books.js

---
+++ src/data/books.js
@@ -0,0 +1,22 @@
+export const books = [
+  {
+    id: '1',
+    title: 'Robot Dreams',
+    author: '1'
+  },
+  {
+    id: '2',
+    title: 'The Starts, Like Dust',
+    author: '1'
+  },
+  {
+    id: '3',
+    title: 'Digital Fortress',
+    author: '2'
+  },
+  {
+    id: '4',
+    title: 'The Three Mosketeers',
+    author: '3'
+  }
+]
