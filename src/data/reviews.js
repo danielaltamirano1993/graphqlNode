edca3543d3bfe3579797525328fd3ed1e1213810
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-05-23 23:32:00 -0500

    Create reviews.js

---
+++ src/data/reviews.js
@@ -0,0 +1,22 @@
+export const reviews = [
+  {
+    id: '1',
+    text: 'Awesome Book!',
+    book: '1' 
+  },
+  {
+    id: '2',
+    text: 'Great Book!',
+    book: '1'
+  },
+  {
+    id: '3',
+    text: 'Amazing Book!',
+    book: '2'
+  },
+  {
+    id: '4',
+    text: 'Fantastic Book!',
+    book: '3'
+  }
+]
